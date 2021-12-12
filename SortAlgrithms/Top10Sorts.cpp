2c7a6c4368fdf78d8d1cf7c20600380e60c5e893
chrispeng <chris19990306@gmail.com>
2021-12-12 15:15:18 +0800

    updateTest

---
+++ SortAlgrithms/Top10Sorts.cpp
@@ -0,0 +1,11 @@
+//
+// Created by chris on 2021/12/12.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "hello world!" << endl;
+    return 0;
+}
---
+++ cmake-build-debug/CMakeFiles/Top10Sorts.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Top10Sorts.dir/SortAlgrithms/Top10Sorts.cpp.o"
+  "Top10Sorts"
+  "Top10Sorts.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Top10Sorts.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
